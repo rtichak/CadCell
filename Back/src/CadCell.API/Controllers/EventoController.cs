1a326fafc42f750cfe60335345590fcba998ca4a
rtichak <ronaldo.tichak@gmail.com>
2023-02-17 15:33:57 -0300

    projeto inicio

---
+++ Back/src/CadCell.API/Controllers/EventoController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CadCell.API.model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace CadCell.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public Evento Get()
+        {
+            return new Evento(){
+                EventoId = 1,
+                Cliente = "Ronaldo",
+                Data = DateTime.Now.AddDays(2).ToString(),
+                Endereco = "Rua 4",
+                Equipamento = "celular",
+                ImageURL ="foto.png"
+            };
+        }
+
+        [HttpPost]
+        public string Post()
+        {
+            return "exemplo de post";
+        }
+
+        [HttpPut("{id}")]
+        public string Put(int id)
+        {
+            return "exemplo de Put";
+        }
+
+        [HttpDelete("{id}")]
+        public string Delete(int id)
+        {
+            return "exemplo de delete";
+        }
+    }
+}
---
+++ Back/src/CadCell.API/model/Evento.cs
@@ -0,0 +1,18 @@
+namespace CadCell.API.model
+{
+    public class Evento
+    {
+        public int EventoId { get; set; }
+
+        public string Cliente { get; set; }
+
+        public string Data { get; set; }
+
+        public string Endereco { get; set; }
+
+        public string Equipamento { get; set; }
+
+        public string ImageURL { get; set; }
+
+    }
+}
