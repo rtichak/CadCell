1a563534c622f44337e238388781028fb72171de
rtichak <ronaldo.tichak@gmail.com>
2023-02-23 16:04:52 -0300

    inclusão do método evendo/id

--- Back/src/CadCell.API/Controllers/EventoController.cs
+++ Back/src/CadCell.API/Controllers/EventoController.cs
@@ -12,22 +12,39 @@ namespace CadCell.API.Controllers
     [Route("[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
-        {
-
-        }
-
-        [HttpGet]
-        public Evento Get()
-        {
-            return new Evento(){
+        public IEnumerable<Evento> _evento = new Evento[]{
+             new Evento (){
                 EventoId = 1,
                 Cliente = "Ronaldo",
                 Data = DateTime.Now.AddDays(2).ToString(),
                 Endereco = "Rua 4",
                 Equipamento = "celular",
                 ImageURL ="foto.png"
-            };
+              },
+              new Evento (){
+                EventoId = 2,
+                Cliente = "Elaine",
+                Data = DateTime.Now.AddDays(2).ToString(),
+                Endereco = "Rua 4",
+                Equipamento = "celular",
+                ImageURL ="foto.png"
+              }
+            };    
+        public EventoController()
+        {
+
+        }
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return _evento;
+        }
+
+        [HttpGet ("{id}")]
+        public IEnumerable<Evento> GetById(int id)
+        {
+            return _evento.Where(evento => evento.EventoId == id);
         }
 
         [HttpPost]
