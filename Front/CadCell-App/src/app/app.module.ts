58ef91c487fc0fbc5bb7ec2598e63b695933ac4e
rtichak <ronaldo.tichak@gmail.com>
2023-03-21 18:16:22 -0300

    organização layout

--- Front/CadCell-App/src/app/app.module.ts
+++ Front/CadCell-App/src/app/app.module.ts
@@ -1,22 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http'
+import { FormsModule } from '@angular/forms';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EventosComponent } from './eventos/eventos.component';
 import { EquipamentosComponent } from './equipamentos/equipamentos.component';
+import { NavComponent } from './nav/nav.component';
+import { CollapseModule } from 'ngx-bootstrap/collapse';
 
 @NgModule({
   declarations: [
     AppComponent,
     EventosComponent,
-    EquipamentosComponent
-  ],
+    EquipamentosComponent,
+      NavComponent
+   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
-    BrowserModule
+    BrowserAnimationsModule,
+    CollapseModule.forRoot(),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Front/CadCell-App/src/app/eventos/eventos.component.ts
+++ Front/CadCell-App/src/app/eventos/eventos.component.ts
@@ -8,7 +8,25 @@ import { HttpClient } from '@angular/common/http';
 })
 export class EventosComponent implements OnInit {
 
-  public eventos: any;
+  public eventos: any = [];
+  private _filtroLista: string = '';
+  public eventosFiltrados: any = [];
+
+  public get filtroLista() {
+    return this._filtroLista;
+  }
+  public set filtroLista(value: string){
+    this._filtroLista = value;
+    this.eventosFiltrados = this.filtroLista ? this.filtrarEventos(this.filtroLista) : this.eventos;
+  }
+
+  filtrarEventos(filtrarPor: string): any{
+    filtrarPor = filtrarPor.toLocaleLowerCase();
+    return this.eventos.filter(
+      (evento: { cliente: string; equipamentos: string; }) => evento.cliente.toLocaleLowerCase().indexOf(filtrarPor)!== -1 ||
+      evento.equipamentos.toLocaleLowerCase().indexOf(filtrarPor) !== -1
+    )
+  }
 
   constructor(private http: HttpClient) { }
 
@@ -20,7 +38,10 @@ export class EventosComponent implements OnInit {
 
   public getEventos(): void {
     this.http.get('https://localhost:5001/Evento').subscribe(
-      response => this.eventos = response,
+      response => {
+        this.eventos = response;
+        this.eventosFiltrados = this.eventos
+      },
       error => console.log(error)
     );
   }
